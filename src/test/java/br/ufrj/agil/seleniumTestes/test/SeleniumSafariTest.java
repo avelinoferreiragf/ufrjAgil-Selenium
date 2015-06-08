1065dc75ed59e9eceb4930597d233e23ba160ba8
AVELINO FERREIRA GOMES FILHO <avelinoferreiragf@gmail.com>
2015-06-08 12:02:54 -0300

    Safari Test

--- src/test/java/br/ufrj/agil/seleniumTestes/test/SeleniumSafariTest.java
+++ src/test/java/br/ufrj/agil/seleniumTestes/test/SeleniumSafariTest.java
@@ -1,23 +1,19 @@
 package br.ufrj.agil.seleniumTestes.test;
 
-import java.io.File;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
-public class SeleniumChrome  {
+public class SeleniumSafariTest  {
 
 	private final Long SLEEP_TIME = 5000L;
-	private final String BROWSER_FILE_PATH = new File("drivers/chromedriver").getAbsolutePath();
 	private WebDriver driver = null;
 
 	@Before
 	public void setUp() {
-		 System.setProperty("webdriver.chrome.driver", BROWSER_FILE_PATH);
-		 this.driver = new ChromeDriver();
+		 this.driver = new SafariDriver();
 	}
 	
 	@Test
