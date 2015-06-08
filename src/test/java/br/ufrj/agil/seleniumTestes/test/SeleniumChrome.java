7bf7228995ab8c2ae49068c0e533d4a9b3138e97
AVELINO FERREIRA GOMES FILHO <avelinoferreiragf@gmail.com>
2015-06-08 12:06:36 -0300

    remoção dos testes que necessitam de tela de browser.

--- src/test/java/br/ufrj/agil/seleniumTestes/test/SeleniumChrome.java
+++ src/test/java/br/ufrj/agil/seleniumTestes/test/SeleniumChrome.java
@@ -1,19 +1,23 @@
 package br.ufrj.agil.seleniumTestes.test;
 
+import java.io.File;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SeleniumSafariTest  {
+public class SeleniumChrome  {
 
 	private final Long SLEEP_TIME = 5000L;
+	private final String BROWSER_FILE_PATH = new File("drivers/chromedriver").getAbsolutePath();
 	private WebDriver driver = null;
 
 	@Before
 	public void setUp() {
-		 this.driver = new SafariDriver();
+		 System.setProperty("webdriver.chrome.driver", BROWSER_FILE_PATH);
+		 this.driver = new ChromeDriver();
 	}
 	
 	@Test
